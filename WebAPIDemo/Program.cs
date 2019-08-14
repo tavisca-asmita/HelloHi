ef8ec28f2fead9432296cd583244aaa9ac2c189e
Asmita Sharma <assharma@affinionds.com>
2019-08-14 19:43:49 +0530

    Url added

--- WebAPIDemo/Program.cs
+++ WebAPIDemo/Program.cs
@@ -19,6 +19,7 @@ namespace WebAPIDemo
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls(urls:"http://*:6001");
     }
 }
